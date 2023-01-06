b153c7d40829e76b913abbb7bf7fc8135fd82154
thorlrsn <thorlarsen96@gmail.com>
2023-01-06 10:33:03 +0100

    new file:   Paws4ever/.gitignore 	new file:   Paws4ever/LICENSE 	new file:   Paws4ever/Makefile 	new file:   Paws4ever/README.md 	new file:   Paws4ever/docs/Makefile

---
+++ Paws4ever/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Classifier of dog species',
+    author='Group13',
+    license='MIT',
+)
