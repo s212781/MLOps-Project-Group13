7dfb4734f3c81bc505cac6468cb9bf4e9b244b9b
cemrizalar1 <s212383@student.dtu.dk>
2023-01-13 15:57:00 +0100

    tests

--- Paws4ever/tests/test_data.py
+++ Paws4ever/tests/test_data.py
@@ -6,7 +6,7 @@ import numpy as np
 N_train = 0
 N_test = 0
 
-data_shape = [0, 0, 0]
+data_shape = 0, 0, 0
 
 num_labels = 120
 
@@ -19,13 +19,10 @@ labels = None
 class DataTest:
 
     def test_data_len():
-        assert len(train_data) == N_train and len(test_data) == N_test
+        assert len(train_data) == N_train and len(test_data) == N_test, "Dataset did not have the correct number of samples"
 
     def test_data_shape():
-        for i in train_data:
-            assert i.shape == data_shape
-        for i in test_data:
-            assert i.shape == data_shape
+        assert train_data.shape == (data_shape) and test_data.shape == (data_shape), "Dataset did not have the correct shape"
 
     def test_labels():
-        assert np.unique(labels) == num_labels
+        assert np.unique(labels) == num_labels, "Labels did not have the correct number"
--- Paws4ever/tests/test_model.py
+++ Paws4ever/tests/test_model.py
@@ -1,7 +1,14 @@
 import pytest
+import torch
+from src.models import train_model
 
+##TO BE FILLED
+model_input_shape = 1,1,1,1
+model_output_shape = 1,1
+
+model = train_model()
 
 class TestModel:
 
     def test_model():
-        pass
+        assert model(torch.rand(model_input_shape)) == (model_output_shape), "Model input and output shape do not match"
