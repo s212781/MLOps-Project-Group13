96fd766df8c1065e32e0f628794df4ddacd797ef
thorlrsn <thorlarsen96@gmail.com>
2023-01-16 11:03:25 +0100

    added print device statement

--- main.py
+++ main.py
@@ -149,6 +149,7 @@ def evaluate(model, val_loader):
 
 if __name__ == "__main__": 
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    print(device)
     dataset = ImageFolder('data/external/images/Images')
     model = DogBreedPretrainedResnet34()
     model.to(device)
