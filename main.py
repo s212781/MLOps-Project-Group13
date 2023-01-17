947750643da0609e5f7412e6edc077f8645571d1
thorlrsn <thorlarsen96@gmail.com>
2023-01-17 16:42:26 +0100

    added print(device)

--- main.py
+++ main.py
@@ -96,8 +96,8 @@ def save_checkpoint(model):
     torch.save(checkpoint, 'model_v1_0.pth')
 
 if __name__ == "__main__":  
-
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    print(device)
     model = create_model()
     model.to(device)
 
