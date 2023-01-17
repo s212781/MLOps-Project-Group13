59a6d865ae49d3b3b638b9d4cc47dce53ad84fc7
thorlrsn <thorlarsen96@gmail.com>
2023-01-17 16:30:35 +0100

    rewritten scripts, awaiting testing on gcloud server

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -1,60 +1,21 @@
-###################################################################
-# Script to convert data to something the frameworks can work with#
-###################################################################
+# ##################################################################
+# # Script to convert data to something the frameworks can work with#
+# ###################################################################
 import numpy as np
-import torch
-import os
-from torch.utils.data import Dataset, DataLoader
-from torchvision import datasets, transforms
-from torchvision.transforms import ToTensor
-import dill
-import cv2
-import pytest
+from torch.utils.data import Dataset
 
-from convert_label import create_lbl_lst
-
-
-def mnist(path):
-    """ Using dogs dataset """
-    h = w = 28
-    class MyDataset(Dataset):
-        def __init__(self, path):
-            lbs = []
-            lbs2 = [] #sry
-            imgs = []
-            for c in os.listdir(path):
-                img_path = path+c
-                img = cv2.imread(img_path)
-                img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-                img = cv2.resize(img,(h,w))
-                label = int(c[c.find('n0')+5:c.find('_')])
-                # label = int(img_path[img_path.find('n0')+5:img_path.find('_')])
-
-                lbs.append(label)
-                imgs.append(img)
-            
-            label_dict = create_lbl_lst()
-
-            for i in range(len(lbs)):
-                lbs2.append(label_dict.index(lbs[i]))
-
-            self.images = torch.tensor(imgs).reshape(-1,1,h,w)
-            self.labels = torch.tensor(lbs2)
-       
-        def __len__(self):
-            return self.images.shape[0]
-
-        def __getitem__(self, idx):
-            return self.images[idx], self.labels[idx]
+""" Using corrupted dataset """
+class MyDataset(Dataset):
+    def __init__(self, ds, transform=None):
+        self.ds = ds
+        self.transform = transform
+        
+    def __len__(self):
+        return len(self.ds)
     
-    data_set = MyDataset(path)
+    def __getitem__(self, idx):
+        img, label = self.ds[idx]
+        if self.transform:
+            img = self.transform(img)  
+            return img, label
 
-    return data_set
-
-if __name__ == "__main__":
-    path = "data/external/images/all_cropped/"
-    data_set = mnist(path)
-
-    output_path = "data/processed/"
-
-    torch.save(data_set, output_path + 'data_tensor.pt', pickle_protocol=True, pickle_module=dill)
--- src/models/train_model.py
+++ src/models/train_model.py
@@ -1,12 +1,7 @@
 import torch
-import wandb
-
 from src.models.predict_model import validation
 
-def train(model, trainloader, testloader, criterion, optimizer=None, epochs=5, print_every=40, wandb_log=False):
-    # if optimizer is None:
-    #     optimizer = torch.optim.Adam(model.parameters(), lr=1e-2)
-    
+def train(model, trainloader, testloader, criterion, optimizer=None, epochs=5, print_every=40):
     steps = 0
     running_loss = 0
     loss_total = []    
@@ -48,13 +43,7 @@ def train(model, trainloader, testloader, criterion, optimizer=None, epochs=5, p
                       "Training Loss: {:.3f}.. ".format(running_loss/print_every),
                       "Test Loss: {:.3f}.. ".format(test_loss/len(testloader)),
                       "Test Accuracy: {:.3f}".format(accuracy/len(testloader)))
-                
-                if wandb_log:
-                    # wandb logging
-                    wandb.log({"Training Loss:": (running_loss/print_every),
-                        "Test Loss:": (test_loss/len(testloader)),
-                        "Test Accuracy:": (accuracy/len(testloader))})
-                    
+                                    
                 running_loss = 0
                 
                 # Make suredropout and grads are on for training
