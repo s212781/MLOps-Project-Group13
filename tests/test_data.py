7d0a6b79be960979525e19f522df5ebb82593915
hoffmannc <hoffmann.colin@yahoo.de>
2023-01-18 13:41:08 +0100

    datatests

--- tests/test_data.py
+++ tests/test_data.py
@@ -1,24 +1,14 @@
-import pytest
-import sys
 import os
-
-sys.path.append(os.path.abspath(os.path.join("..", "main")))
-from main import load_data
+from torchvision.datasets import ImageFolder
 
 
 def test_data():
-    train_dataset, valid_dataset = load_data()
-    for image, label in train_dataset:
-        with pytest.raises(ValueError, match="Expected input to a 4D tensor"):
-            image.shape == [1, 3, 224, 224]
-        with pytest.raises(ValueError, match="Expected input to be a 2D tensor"):
-            label.shape == [1, 1]
 
-    for image, label in valid_dataset:
-        with pytest.raises(ValueError, match="Expected input to a 4D tensor"):
-            image.shape == [1, 3, 224, 224]
-        with pytest.raises(ValueError, match="Expected input to be a 2D tensor"):
-            label.shape == [1, 1]
+    currentfolder = os.getcwd()
+    dataset = ImageFolder(currentfolder + "/data/processed/images")
 
+    for image, label in dataset:
+        assert image.mode == "RGB", "Unexpected image mode"
+        assert label in dataset.targets, "Unexpected label"
 
-test_data()
+    assert len(dataset) == 20580, "Unexpected dataset length"
