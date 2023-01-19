3ee546b2ef984b4091d09c100abd5b3c37ebdcc7
thorlrsn <thorlarsen96@gmail.com>
2023-01-19 21:06:20 +0100

    adding linux image

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,4 @@
 dvc pull data_lite
 wandb login c2dcb8a6504297b59ce85d4d01a9d560eb1f0934
 # python -u src/data/make_dataset.py preprocess-command --num_samples=10
-sudo apt-get -y install curl
-curl https://raw.githubusercontent.com/GoogleCloudPlatform/compute-gpu-installation/main/linux/install_gpu_driver.py --output install_gpu_driver.py
-python3 install_gpu_driver.py
 python3 -u main.py
