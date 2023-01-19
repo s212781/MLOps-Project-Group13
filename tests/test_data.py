93c89455a46ae1252f8dbb28454f1521ee3459a8
thorlrsn <thorlarsen96@gmail.com>
2023-01-19 13:45:27 +0100

    adding test scripts

---
+++ tests/test_data.py
@@ -0,0 +1,27 @@
+import os
+
+print(os.getcwd())
+from torchvision.datasets import ImageFolder
+
+_TEST_ROOT = os.path.dirname(__file__)  # root of test folder
+_PROJECT_ROOT = os.path.dirname(_TEST_ROOT)  # root of project
+_PATH_DATA = os.path.join(_PROJECT_ROOT, "data")  # root of data
+
+
+def test_data():
+    """
+    Test data for model training.
+
+    Checking:
+    - image mode
+    - label
+    - dataset length
+    """
+
+    dataset = ImageFolder(_PATH_DATA + "/processed/images")
+
+    for image, label in dataset:
+        assert image.mode == "RGB", "Unexpected image mode"
+        assert label in dataset.targets, "Unexpected label"
+
+    assert len(dataset) == 20580, "Unexpected dataset length"
---
+++ tests/test_model.py
@@ -0,0 +1,14 @@
+import torch
+import torch.nn as nn
+import torchvision.models as models
+
+
+def test_model():
+    model = models.resnet152(pretrained=True)
+    num_ftrs = model.fc.in_features
+
+    model.fc = nn.Linear(num_ftrs, 120)
+
+    input = torch.zeros((1, 3, 228, 228))
+    output = model(input)
+    assert output.shape == torch.Size([1, 120])
